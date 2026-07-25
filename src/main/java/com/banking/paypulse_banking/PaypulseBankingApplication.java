c0000ba7cc128404dbd300a178de5c5b448c95b2
Heshan Nawarathna <117019222+heshannawarathne@users.noreply.github.com>
2026-07-25 16:07:39 +0530

    setupProject

---
+++ src/main/java/com/banking/paypulse_banking/PaypulseBankingApplication.java
@@ -0,0 +1,13 @@
+package com.banking.paypulse_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaypulseBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaypulseBankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/paypulse_banking/PaypulseBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.paypulse_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaypulseBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
